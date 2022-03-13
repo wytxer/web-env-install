4b0205540a90a872a3d7fa34fd01838e6ef34e07
wytxer <wy@bszhct.com>
2022-03-13 16:03:08 +0800

    refactor: 名称大写

--- install.sh
+++ install.sh
@@ -2,24 +2,24 @@
 
 echo "> 脚本加载中..."
 
-# 安装 zsh
+# 安装 ZSH
 if hash zsh 2>/dev/null; then
-  echo "> zsh 已存在"
+  echo "> ZSH 已存在"
 else
-  echo "> 开始安装 zsh..."
+  echo "> 开始安装 ZSH..."
   # 可能会遇到报错：curl: (35) LibreSSL SSL_connect: SSL_ERROR_SYSCALL in connection to raw.githubusercontent.com:443，推荐使用 gitee 的
   # sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
-  # zsh 国内
+  # ZSH 国内
   sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)" "" --unattended
 fi
 
-# 安装 nvm
+# 安装 NVM
 if [ -f "${HOME}/.nvm/nvm.sh" ]; then
-  echo "> nvm 已存在"
+  echo "> NVM 已存在"
 else
   # 备份全局的 npm 包
   if hash npm 2>/dev/null; then
-    echo "> 开始备份全局的 npm 包..."
+    echo "> 开始备份全局的 NPM 包..."
     packInfo=$(npm ls -g --depth=0)
     index=0
     npmPackages=()
@@ -38,11 +38,11 @@ else
     cd  /usr/local/bin && ls -l | grep "../lib/node_modules/" | awk '{print $9}'| xargs rm
   fi
 
-  echo "> 开始安装 nvm..."
+  echo "> 开始安装 NVM..."
   curl -o- https://gitee.com/mirrors/nvm/raw/master/install.sh | bash
-  echo "  配置 nvm"
+  echo "  配置 NVM"
   echo "" >> ~/.zshrc
-  echo "# nvm" >> ~/.zshrc
+  echo "# NVM" >> ~/.zshrc
   echo "export NVM_DIR=\"\$([ -z \"\${XDG_CONFIG_HOME-}\" ] && printf %s \"\${HOME}/.nvm\" || printf %s \"\${XDG_CONFIG_HOME}/nvm\")\"" >> ~/.zshrc
   echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"" >> ~/.zshrc
 
@@ -55,19 +55,19 @@ else
   nvm alias default v16.10.0
 fi
 
-# 安装 nrm
+# 安装 NRM
 if hash nrm 2>/dev/null; then
-  echo "> nrm 已存在"
+  echo "> NRM 已存在"
 else
-  echo "> 开始安装 nrm..."
+  echo "> 开始安装 NRM..."
   npm i nrm -g
 fi
 
-# 安装 yarn
+# 安装 Yarn
 if hash yarn 2>/dev/null; then
-  echo "> yarn 已存在"
+  echo "> Yarn 已存在"
 else
-  echo "> 开始安装 yarn..."
+  echo "> 开始安装 Yarn..."
   npm i yarn -g
 fi
 
